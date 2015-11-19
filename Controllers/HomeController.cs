ed2edb666390cd4ab9f18530a3d81e81cc9e9c55
Rafael Santos <rafa.santos.24@unitec.edu>
2015-11-19 00:47:46 -0600

    Cambios en el Home, login, registrar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace HackProElite.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View("Login");
         }
     }
 }
